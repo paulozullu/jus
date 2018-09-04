6d3398c6b8104833ad4c55bde1bfab345bb6131e
paulozullu <paulozullu@gmail.com>
2018-09-03 23:29:22 -0300

    Created templates / Added DB settings

--- jus/settings.py
+++ jus/settings.py
@@ -15,6 +15,8 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+MONGO_URI = 'mongodb://localhost:27017'
+MONGO_DATABASE = 'jusbrasil_test'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
--- jus/urls.py
+++ jus/urls.py
@@ -13,9 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', TemplateView.as_view(template_name='search_process.html'), name='search_process'),
+    url(r'^', include('jus_web.urls', namespace='jus_web')),
 ]
