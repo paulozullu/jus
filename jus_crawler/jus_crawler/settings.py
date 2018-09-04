a4394dac0aa467365aebb1c9c9ff3ce205ef35f1
paulozullu <paulozullu@gmail.com>
2018-09-03 22:51:24 -0300

    Ignore robots.txt

--- jus_crawler/jus_crawler/settings.py
+++ jus_crawler/jus_crawler/settings.py
@@ -19,7 +19,7 @@ NEWSPIDER_MODULE = 'jus_crawler.spiders'
 #USER_AGENT = 'jus_crawler_ (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
